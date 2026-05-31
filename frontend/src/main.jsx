92863e00b41b9fca1c47030bea3ea766f6e57f5f
AYUSH420-g <99ayushsoni@gmail.com>
2026-06-01 02:40:03 +0530

    auth page and shop page changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import App from "./app/App.jsx";
 import "./styles/index.css";
 
@@ -8,5 +9,6 @@ createRoot(document.getElementById("root")).render(
   <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || "716866073371-50ja4kc6r43al6f4k0utd7m206kvjtiv.apps.googleusercontent.com"}>
     <App />
      <Analytics />
+     <SpeedInsights />
   </GoogleOAuthProvider>
 );
