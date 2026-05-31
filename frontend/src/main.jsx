650f5b193c126a67d917bf0ec5d3669b499c95c6
AYUSH420-g <99ayushsoni@gmail.com>
2026-05-31 17:13:17 +0530

    new deploy code

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./app/App.jsx";
 import "./styles/index.css";
+
 createRoot(document.getElementById("root")).render(
   <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || "716866073371-50ja4kc6r43al6f4k0utd7m206kvjtiv.apps.googleusercontent.com"}>
     <App />
+     <Analytics />
   </GoogleOAuthProvider>
 );
